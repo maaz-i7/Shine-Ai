fd2b32f543f3c3836ce2af4861370b4e622eadab
maaz-i7 <maaz.khan.sdr@gmail.com>
2026-07-10 21:24:22 +0530

    added progress comp on homepage

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import heroBg from "../../public/images/hero-bg.png"
+import ProgressJourney from "@/components/ProgressJourney";
 
 export default function Home() {
   return (
     <main className="h-1000">
-      <div className="w-screen flex items-center justify-center py-15">
-        <Image loading="eager" className="w-150" src={heroBg} alt="logo" />
+      <div className="w-screen flex items-center justify-center">
+        <Image loading="eager" className="w-200" src={heroBg} alt="logo" />
       </div>
-      <div className="w-screen flex justify-center">
-        <button className="bg-blue-500 text-2xl w-50 font-bold p-4 px-6 m-5 rounded-4xl cursor-pointer transition-colors duration-300 hover:bg-blue-600">&lt;Canvas/&gt;</button>
-        <button className="bg-yellow-500 text-2xl w-50 font-bold p-4 px-6 m-5 rounded-4xl cursor-pointer transition-colors duration-300 hover:bg-yellow-600">Register</button>
+      <ProgressJourney/>
+      <div className="w-screen flex justify-center mt-10">
+        <button className="bg-blue-500 text-2xl w-50 font-bold p-4 px-6 m-5 rounded-xl cursor-pointer transition-colors duration-300 hover:bg-blue-600">&lt;Canvas/&gt;</button>
+        <button className="bg-yellow-500 text-2xl w-50 font-bold p-4 px-6 m-5 rounded-xl cursor-pointer transition-colors duration-300 hover:bg-yellow-600">Register</button>
       </div>
     </main>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,9 +3,9 @@ import logo from "../../public/images/logo-no-bg.png";
 
 export default function Navbar() {
     return (
-        <nav className="w-screen bg-black/60 backdrop-blur-md border-b border-white/10 flex items-center justify-between top-0 sticky p-5">
+        <nav className="w-screen bg-black/60 backdrop-blur-md border-b border-white/10 flex items-center justify-between top-0 sticky">
             <div className="">
-                <Image loading="eager" className="w-10" src={logo} alt="logo" />
+                <Image loading="eager" className="w-15" src={logo} alt="logo" />
             </div>
             <div>
                 <ul className="flex w-80 justify-around items-center">
